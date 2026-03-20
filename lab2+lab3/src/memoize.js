f3668402d74c8ecd9c20350f197bf662f48878f9
Bbg0kif <b.oleksandra200@gmail.com>
2026-03-20 14:42:22 +0200

    implement basic memoization and cache lookup

--- lab2+lab3/src/memoize.js
+++ lab2+lab3/src/memoize.js
@@ -6,4 +6,17 @@ export function memoize(fn, options = {}) {
     customPolicy = null
   } = options;
 
+  const cache = new Map();
+
+  return function (...args) {
+    const key = JSON.stringify(args);
+
+    if (cache.has(key)) {
+      return cache.get(key);
+    }
+
+    const result = fn(...args);
+    cache.set(key, result);
+    return result;
+  };
 }
