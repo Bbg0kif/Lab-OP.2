77e8eefbb03063e49011646c08133d3516d9d709
Bbg0kif <b.oleksandra200@gmail.com>
2026-04-04 22:04:36 +0300

    implement internal index lookup and peek method

--- lab4/src/priorityQueue.js
+++ lab4/src/priorityQueue.js
@@ -10,4 +10,27 @@ export class PriorityQueue {
       id: this._counter++
     });
   }
+  _findIndex(type) {
+    if (this.items.length === 0) return -1;
+
+    return this.items.reduce((bestIdx, curr, currIdx, arr) => {
+      const best = arr[bestIdx];
+      switch (type) {
+        case 'highest': 
+          return curr.priority > best.priority ? currIdx : bestIdx;
+        case 'lowest':  
+          return curr.priority < best.priority ? currIdx : bestIdx;
+        case 'oldest':  
+          return curr.id < best.id ? currIdx : bestIdx;
+        case 'newest':  
+          return curr.id > best.id ? currIdx : bestIdx;
+        default: 
+          return bestIdx;
+      }
+    }, 0);
+  }
+  peek(type) {
+    const idx = this._findIndex(type);
+    return idx !== -1 ? this.items[idx].item : null;
+  }
 }
