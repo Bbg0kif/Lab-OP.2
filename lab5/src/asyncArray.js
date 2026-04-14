8cd6476d1d884af3e281fd95928f23228d363f52
Bbg0kif <b.oleksandra200@gmail.com>
2026-04-14 13:52:04 +0300

    add promise-based map with AbortController support to AsyncArray

--- lab5/src/asyncArray.js
+++ lab5/src/asyncArray.js
@@ -3,9 +3,7 @@ export const AsyncArray = {
     const results = [];
     let completed = 0;
 
-    if (array.length === 0) {
-      return callback([]);
-    }
+    if (array.length === 0) return callback([]);
 
     array.forEach((item, index) => {
       setTimeout(() => {
@@ -16,6 +14,38 @@ export const AsyncArray = {
           callback(results);
         }
       }, 0);
+    });
+  },
+  
+  mapPromise(array, fn, { signal } = {}) {
+    return new Promise((resolve, reject) => {
+      if (signal?.aborted) {
+        return reject(new Error('Operation aborted by user'));
+      }
+
+      const results = [];
+      let completed = 0;
+
+      if (array.length === 0) return resolve([]);
+
+      array.forEach((item, index) => {
+        setTimeout(() => {
+          if (signal?.aborted) {
+            return reject(new Error('Operation aborted by user'));
+          }
+
+          results[index] = fn(item);
+          completed++;
+
+          if (completed === array.length) {
+            resolve(results);
+          }
+        }, 100);
+      });
+
+      signal?.addEventListener('abort', () => {
+        reject(new Error('Operation aborted by user'));
+      });
     });
   }
 };
